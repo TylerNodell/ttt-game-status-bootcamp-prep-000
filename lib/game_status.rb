70852865e613150d6bfd55252af76bad23d7b39e
TylerNodell <tyler.l.nodell@gmail.com>
2018-06-30 16:14:05 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,3 +14,18 @@ WIN_COMBINATIONS = [
   [0,4,8],
   [6,4,2]
 ]
+
+def won?(board)
+  board_combinations = [
+    [board[0],board[1],board[2]],
+    [board[3],board[4],board[5]],
+    [board[6],board[7],board[8]],
+    [board[0],board[3],board[6]],
+    [board[1],board[4],board[7]],
+    [board[2],board[5],board[8]],
+    [board[0],board[4],board[8]],
+    [board[6],board[4],board[2]]
+  ]
+  
+  board_combinations.any?{|i| i = WIN_COMBINATIONS[i]}
+end
